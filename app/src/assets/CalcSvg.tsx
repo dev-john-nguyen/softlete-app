40b6e7428db27c018955911ecffeacbc068c01be
John Nguyen <36948494+nguyening20@users.noreply.github.com>
2023-01-31 20:21:56 -0800

    Revert "Revert "config""
    This reverts commit ef0763cb6fe9116b0d107066fc3fa8aa0af5c66a.

--- app/src/assets/CalcSvg.tsx
+++ app/src/assets/CalcSvg.tsx
@@ -10,18 +10,25 @@ const svg = `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/
 <path d="M8 17V17.01"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M12 17V17.01"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M16 17V17.01"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>`
+</svg>`;
 
 interface Props {
-    strokeColor: string;
+  strokeColor?: string;
+  size?: number;
+  color?: string;
 }
 
-class CalcSvg extends React.Component<Props, {}> {
-    render() {
-        return (
-            <SvgXml xml={svg} width='100%' height='100%' stroke={this.props.strokeColor} />
-        )
-    }
+class CalcSvg extends React.Component<Props> {
+  render() {
+    return (
+      <SvgXml
+        xml={svg}
+        width={this.props.size || '100%'}
+        height={this.props.size || '100%'}
+        stroke={this.props.color || this.props.strokeColor}
+      />
+    );
+  }
 }
 
-export default CalcSvg
+export default CalcSvg;
--- app/src/components/workout/Navbar.tsx
+++ app/src/components/workout/Navbar.tsx
@@ -2,87 +2,123 @@ import React from 'react';
 import { Pressable, ScrollView, StyleSheet, View } from 'react-native';
 import { normalize } from '../../utils/tools';
 import { WorkoutStatus } from '../../services/workout/types';
-import NavbarItem from './NavbarItem';
-import BaseColors, { rgba } from '../../utils/BaseColors';
-import PlusSvg from '../../assets/PlusSvg';
-import StyleConstants from '../tools/StyleConstants';
+import { FlexBox } from '@app/ui';
+import { Colors, moderateScale, rgba, StyleConstants } from '@app/utils';
+import { CircleAdd } from '@app/elements';
+import Animated, {
+  useAnimatedStyle,
+  withTiming,
+} from 'react-native-reanimated';
+
+interface NavbarItemProps {
+  index: number;
+  curGroup: number;
+  onPress: () => void;
+  color: string;
+  lightColor: string;
+}
+
+const activeWidth = normalize.width(20);
+const inactiveWidth = normalize.width(30);
+
+const NavbarItem = ({
+  index,
+  curGroup,
+  onPress,
+  color,
+  lightColor,
+}: NavbarItemProps) => {
+  const animatedStyles = useAnimatedStyle(() => {
+    const active = curGroup * 30 === index * 30 ? true : false;
+    return {
+      backgroundColor: active ? withTiming(color) : withTiming(lightColor),
+      height: active ? withTiming(activeWidth) : withTiming(inactiveWidth),
+      width: active ? withTiming(activeWidth) : withTiming(inactiveWidth),
+      borderRadius: 100,
+      marginRight: 20,
+    };
+  }, [curGroup, color]);
+
+  return (
+    <Pressable style={styles.itemContainer} onPress={onPress} hitSlop={5}>
+      <Animated.View key={index} style={animatedStyles} />
+    </Pressable>
+  );
+};
 
 interface Props {
-    groupKeys: number[];
-    onGroupPress: (key: number) => void;
-    curGroup: number;
-    onAddExercise: (newGroup: boolean) => void;
-    status: WorkoutStatus;
-    athlete?: boolean;
+  groupKeys: number[];
+  onGroupPress: (key: number) => void;
+  curGroup: number;
+  onAddExercise: (newGroup: boolean) => void;
+  status: WorkoutStatus;
+  athlete?: boolean;
 }
 
-//I know the width
-//How to determine the pos and overlap 
-
-const WorkoutNavbar = ({ groupKeys, onGroupPress, curGroup, onAddExercise, status, athlete }: Props) => {
-    return (
-        <ScrollView
-            style={styles.container}
-            contentContainerStyle={styles.contentContainerStyle}
-            horizontal
-        >
-            <View style={styles.groupContainer}>
-                {
-                    status === WorkoutStatus.completed && (
-                        <View style={styles.reflect} />
-                    )
-                }
-                {groupKeys.map((g, index) => <NavbarItem
-                    index={index}
-                    curGroup={curGroup}
-                    key={index}
-                    onPress={() => curGroup === g ? onAddExercise(false) : onGroupPress(g)}
-                    color={status === WorkoutStatus.completed ? BaseColors.green : BaseColors.white}
-                    lightColor={status === WorkoutStatus.completed ? rgba(BaseColors.greenRbg, .5) : rgba(BaseColors.whiteRbg, .5)}
-                />)}
-                {
-                    status === WorkoutStatus.inProgress && (
-                        <View style={styles.reflect} />
-                    )
-                }
-                {
-                    (!athlete && status !== WorkoutStatus.completed) && (
-                        <Pressable style={styles.plus} onPress={() => onAddExercise(true)}>
-                            <PlusSvg strokeColor={BaseColors.white} />
-                        </Pressable>
-                    )
-                }
-            </View>
-        </ScrollView>
-    )
-}
+const WorkoutNavbar = ({
+  groupKeys,
+  onGroupPress,
+  curGroup,
+  onAddExercise,
+  status,
+  athlete,
+}: Props) => {
+  return (
+    <ScrollView style={styles.container} horizontal>
+      <FlexBox alignItems="center">
+        {status === WorkoutStatus.completed && <View style={styles.reflect} />}
+        {groupKeys.map((g, index) => (
+          <NavbarItem
+            index={index}
+            curGroup={curGroup}
+            key={index}
+            onPress={() =>
+              curGroup === g ? onAddExercise(false) : onGroupPress(g)
+            }
+            color={
+              status === WorkoutStatus.completed ? Colors.green : Colors.white
+            }
+            lightColor={
+              status === WorkoutStatus.completed
+                ? rgba(Colors.greenRbg, 0.5)
+                : rgba(Colors.whiteRbg, 0.5)
+            }
+          />
+        ))}
+        {status === WorkoutStatus.inProgress && <View style={styles.reflect} />}
+        {!athlete && status !== WorkoutStatus.completed && (
+          <CircleAdd
+            onPress={() => onAddExercise(true)}
+            size={12}
+            style={{
+              position: 'relative',
+              bottom: 0,
+              padding: moderateScale(5),
+            }}
+          />
+        )}
+      </FlexBox>
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        width: '100%',
-        maxWidth: normalize.width(2),
-        height: '100%',
-    },
-    contentContainerStyle: {
-    },
-    groupContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    plus: {
-        height: normalize.width(15),
-        width: normalize.width(15),
-        borderRadius: 100,
-        padding: 8,
-        backgroundColor: BaseColors.primary
-    },
-    reflect: {
-        height: normalize.width(25),
-        width: normalize.width(25),
-        borderRadius: 100,
-        borderColor: BaseColors.green,
-        borderWidth: 1,
-        marginRight: StyleConstants.baseMargin
-    }
-})
+  container: {
+    width: '100%',
+    maxWidth: normalize.width(2),
+    height: '100%',
+  },
+  itemContainer: {
+    height: normalize.width(20),
+    justifyContent: 'center',
+  },
+  reflect: {
+    height: normalize.width(25),
+    width: normalize.width(25),
+    borderRadius: 100,
+    borderColor: Colors.green,
+    borderWidth: 1,
+    marginRight: StyleConstants.baseMargin,
+  },
+});
 export default WorkoutNavbar;
