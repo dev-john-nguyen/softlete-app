f88781459c581568dbedfe25f2832dd63fe3e084
John Nguyen <36948494+nguyening20@users.noreply.github.com>
2023-02-06 09:38:43 -0800

    reflection

--- app/src/assets/CloseSvg.tsx
+++ app/src/assets/CloseSvg.tsx
@@ -6,7 +6,7 @@ const svg = `<svg  viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
 </svg>`;
 
 interface Props {
-  fillColor: string;
+  fillColor?: string;
   color: string;
   size?: string | number;
 }
