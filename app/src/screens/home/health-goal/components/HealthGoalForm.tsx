7478feba43ebcc6ee7efe39d90d91ba6c55f04e1
John Nguyen <36948494+nguyening20@users.noreply.github.com>
2023-05-14 10:01:51 -0700

    Foundational Screen Config

--- app/src/screens/home/health-goal/components/HealthGoalForm.tsx
+++ app/src/screens/home/health-goal/components/HealthGoalForm.tsx
@@ -1,11 +1,25 @@
-import { PrimaryText, ScreenTemplate } from '@app/elements';
+import {
+  Input,
+  PrimaryButton,
+  PrimaryText,
+  ScreenTemplate,
+} from '@app/elements';
 import { FlexBox } from '@app/ui';
-import React from 'react';
+import React, { useState } from 'react';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const HealthGoalForm = () => {
+  const [sleep, setSleep] = useState(0);
+  const [activeCalories, setActiveCalories] = useState(0);
+
+  const onSave = () => {
+    console.log(sleep, activeCalories);
+  };
+
   return (
     <ScreenTemplate
       isBackVisible
+      applyContentPadding
       leftContentFlex={0}
       rightContentFlex={0}
       middleContent={
@@ -15,7 +29,51 @@ const HealthGoalForm = () => {
           </PrimaryText>
         </FlexBox>
       }>
-      <FlexBox column></FlexBox>
+      <PrimaryText>
+        Challenge yourself to reach your full potential by setting daily sleep
+        and move goals.
+      </PrimaryText>
+      <FlexBox column marginTop={10}>
+        <FlexBox
+          paddingBottom={5}
+          borderBottomWidth={1}
+          borderBottomColor={Colors.white}
+          marginBottom={5}>
+          <PrimaryText variant="primary" size="medium">
+            Sleep
+          </PrimaryText>
+        </FlexBox>
+        <PrimaryText marginBottom={10}>
+          {`It's recommended to sleep 7-9 hours per night.`}
+        </PrimaryText>
+        <Input
+          label="Hours"
+          onChangeText={numStr => setSleep(parseInt(numStr) ?? 0)}
+          defaultValue={String(activeCalories)}
+          keyboardType="numeric"
+        />
+      </FlexBox>
+      <FlexBox column marginTop={10}>
+        <FlexBox
+          paddingBottom={5}
+          borderBottomWidth={1}
+          borderBottomColor={Colors.white}
+          marginBottom={5}>
+          <PrimaryText variant="primary" size="medium">
+            Active Calories
+          </PrimaryText>
+        </FlexBox>
+        <PrimaryText marginBottom={10}>
+          {`We recommend you burn 200 active calories per day.`}
+        </PrimaryText>
+        <Input
+          label="Kcal"
+          onChangeText={numStr => setActiveCalories(parseInt(numStr) ?? 0)}
+          defaultValue={String(sleep)}
+          keyboardType="numeric"
+        />
+      </FlexBox>
+      <PrimaryButton marginTop={30}>Save</PrimaryButton>
     </ScreenTemplate>
   );
 };
