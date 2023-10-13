6903e591a04aaf339d4c75d0115d744391134906
John Nguyen <36948494+nguyening20@users.noreply.github.com>
2023-10-13 06:57:53 -0700

    disconnecting websockets for now

--- app/src/screens/network/Maintenance.tsx
+++ app/src/screens/network/Maintenance.tsx
@@ -7,15 +7,22 @@ import { Colors } from '@app/utils';
 
 const Maintenance = () => {
   return (
-    <ScreenTemplate>
-      <FlexBox flex={1} column alignItems="center" justifyContent="center">
+    <ScreenTemplate applyContentPadding>
+      <FlexBox flex={1} column alignItems="center">
         <PrimaryText bold fontSize={30} variant="primary" marginBottom={5}>
           Softlete Social
         </PrimaryText>
-        <Icon icon="world" size={100} color={Colors.lightWhite} />
-        <PrimaryText bold size="large" marginTop={20}>
-          Coming Soon!
+        <PrimaryText>
+          Cultivate connections with fellow athletes, stay updated on their
+          training regimens, explore and download training programs/workouts
+          from others.
         </PrimaryText>
+        <FlexBox flex={1} column alignItems="center" marginTop={20}>
+          <Icon icon="world" size={120} color={Colors.lightWhite} />
+          <PrimaryText bold fontSize={30} marginTop={20}>
+            Coming Soon!
+          </PrimaryText>
+        </FlexBox>
       </FlexBox>
     </ScreenTemplate>
   );
