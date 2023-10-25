2371c8c9dcb57a53d02ac1891900f790f4203a33
John Nguyen <36948494+nguyening20@users.noreply.github.com>
2023-10-25 07:28:41 -0700

    remove admin config

--- server/src/api/admin/validate-admin.ts
+++ server/src/api/admin/validate-admin.ts
@@ -1,20 +1,23 @@
 import Users from '../../collections/users';
 
 export default function validateAdmin(req: any, res: any, next: any) {
-    const { uid } = req.headers;
-    if (!uid) return res.status(401).send("Cannot find your user id. Authorization rejected.");
+  const { uid } = req.headers;
+  if (!uid)
+    return res
+      .status(401)
+      .send('Cannot find your user id. Authorization rejected.');
 
-    Users.findOne({ uid, admin: true })
-        .then((doc) => {
-            if (doc) {
-                next();
-            } else {
-                res.status(401).send("Not an authorized user.")
-            }
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).send("Unexpected error occurred.")
-        })
-    //fetch admins
+  Users.findOne({ uid, admin: true })
+    .then(doc => {
+      if (doc) {
+        next();
+      } else {
+        res.status(401).send('Not an authorized user.');
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('Unexpected error occurred.');
+    });
+  //fetch admins
 }
