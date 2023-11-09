6cfd8ef2315934ae4962c9faeaec5ed4f878f20f
John Nguyen <36948494+nguyening20@users.noreply.github.com>
2023-11-09 07:04:40 -0800

    disrupt warning

--- app/src/screens/modals/Timer.tsx
+++ app/src/screens/modals/Timer.tsx
@@ -79,10 +79,14 @@ const Timer = () => {
           justifyContent="center"
           backgroundColor={Colors.blendWhite}
           borderRadius={5}
+          maxWidth="80%"
           padding={20}>
-          <FlexBox marginBottom={10}>
+          <FlexBox marginBottom={10} column alignItems="center">
             <PrimaryText variant="primary" size="large">
               Stopwatch
+            </PrimaryText>
+            <PrimaryText>
+              This stopwatch will be disrupted if you leave the app.
             </PrimaryText>
           </FlexBox>
           <FlexBox>
