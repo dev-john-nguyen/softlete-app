5efe835777d2765472ad3f255cf09ac43fecfc8e
John Nguyen <36948494+nguyening20@users.noreply.github.com>
2023-12-21 08:58:00 -0800

    fix dragging position and drop handler

--- app/src/components/restructure/Moveable.tsx
+++ app/src/components/restructure/Moveable.tsx
@@ -98,11 +98,23 @@ const RestructureMoveable = ({
     }
   };
 
+  const onGestureFinishHandler = () => {
+    if (!movingGroup) return;
+
+    if (movingGroup === 'trash') {
+      onTrashExercise(id);
+    } else {
+      onExerciseToGroup(id, movingGroup);
+    }
+  };
+
   const onGestureActive = (
     event: Readonly<GestureEventPayload & PanGestureHandlerEventPayload>,
   ) => {
     'worklet';
-    const { absoluteY, absoluteX } = event;
+    const { absoluteY: eventAbsY, absoluteX } = event;
+
+    const absoluteY = eventAbsY - HEIGHT;
 
     //check if the item is in the header
     if (absoluteY < headerPos.value.y && scrollY.value <= 20) {
@@ -142,27 +154,7 @@ const RestructureMoveable = ({
     event: Readonly<GestureEventPayload & PanGestureHandlerEventPayload>,
   ) => {
     'worklet';
-    //find group it dropped it in
-    const { absoluteY, absoluteX } = event;
-
-    if (absoluteY < headerPos.value.y && scrollY.value <= 20) {
-      const targetGroup = findOverlapGroup(
-        groupsPos,
-        trashPos,
-        absoluteX,
-        headerPos.value.x,
-      );
-
-      if (targetGroup && targetGroup !== curGroup) {
-        //update exercises
-        if (targetGroup === 'trash') {
-          runOnJS(onTrashExercise)(id);
-        } else {
-          runOnJS(onExerciseToGroup)(id, targetGroup);
-        }
-      }
-    }
-
+    runOnJS(onGestureFinishHandler)();
     top.value = exercisePos.value[id] * HEIGHT;
     width.value = withTiming(90);
     left.value = withTiming(leftStart);
