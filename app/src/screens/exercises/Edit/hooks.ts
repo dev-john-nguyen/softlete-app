19a4ccad66f899363e08e243a7b1e9fc85d38618
John Nguyen <36948494+nguyening20@users.noreply.github.com>
2023-12-20 18:18:49 -0800

    Update hooks.ts

--- app/src/screens/exercises/Edit/hooks.ts
+++ app/src/screens/exercises/Edit/hooks.ts
@@ -1,6 +1,6 @@
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { useMutation } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Alert } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { ReducerProps, ThunkAppDispatch } from 'src/services';
@@ -57,15 +57,12 @@ export const useIsOwner = () => {
     targetExercise: state.exercises.targetExercise,
     user: state.user,
   }));
-  const [isOwner, setIsOwner] = useState(true);
-  useEffect(() => {
-    setIsOwner(() => {
-      if (!targetExercise) return false;
-      if (targetExercise.userUid === user.uid) return true;
-      if (targetExercise.softlete && user.admin) return true;
-      if (!targetExercise._id) return true;
-      return false;
-    });
+  const isOwner = useMemo(() => {
+    if (!targetExercise) return false;
+    if (targetExercise.userUid === user.uid) return true;
+    if (targetExercise.softlete && user.admin) return true;
+    if (!targetExercise._id) return true;
+    return false;
   }, [targetExercise, user]);
-  return { isOwner, setIsOwner };
+  return { isOwner };
 };
