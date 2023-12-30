341e48454d81496174291eaa746080df77e9a87e
John Nguyen <36948494+nguyening20@users.noreply.github.com>
2023-12-30 08:09:05 -0800

    disabled color changes

--- app/src/components/elements/Input.tsx
+++ app/src/components/elements/Input.tsx
@@ -80,14 +80,8 @@ const Input = ({
     return {
       fontSize: numbers ? moderateScale(20) : StyleConstants.smallFont,
       paddingTop: multiline ? 10 : undefined,
-      borderBottomColor:
-        editable || editable == null
-          ? rgba(BaseColors.whiteRbg, 0.7)
-          : rgba(BaseColors.whiteRbg, 0.2),
-      color:
-        editable || editable == null
-          ? BaseColors.lightWhite
-          : rgba(BaseColors.lightWhiteRgb, 0.5),
+      borderBottomColor: BaseColors.white,
+      color: BaseColors.white,
       maxHeight: maxHeight,
       ...borderStyles,
     };
@@ -102,6 +96,7 @@ const Input = ({
           marginTop: props.mt,
           marginBottom: props.mb,
           margin: props.m,
+          opacity: editable ? 1 : 0.5,
         },
         containerStyles,
       ]}>
