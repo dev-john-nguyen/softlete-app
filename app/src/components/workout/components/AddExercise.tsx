589635c342a6a852f8f8531fe21b391a6d15cee5
John Nguyen <36948494+nguyening20@users.noreply.github.com>
2024-01-02 08:36:50 -0800

    fix add button background

--- app/src/components/workout/components/AddExercise.tsx
+++ app/src/components/workout/components/AddExercise.tsx
@@ -104,6 +104,8 @@ const AddExercise: FC<Props> = ({ onAddExercise }) => {
   const dropWrapper = useAnimatedStyle(() => {
     return {
       opacity: showOptions ? withTiming(1) : withTiming(0),
+      position: 'absolute',
+      bottom: '100%',
     };
   }, [showOptions]);
 
