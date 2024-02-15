bc0fd6edb39cd28227a6e50d6373897fe3d03c2f
John Nguyen <36948494+nguyening20@users.noreply.github.com>
2024-02-15 09:27:23 -0800

    add option to map

--- app/src/components/workout/endurance/index.tsx
+++ app/src/components/workout/endurance/index.tsx
@@ -6,10 +6,13 @@ import EnduranceSummary from './components/EnduranceSummary';
 import Icon from '@app/icons';
 import { Colors } from '@app/utils';
 import { PrimaryText } from '@app/elements';
+import { HomeStackParamsList, HomeStackScreens } from 'src/screens/home/types';
+import { useNavigation } from '@react-navigation/native';
 
 const EnduranceWrapper = () => {
   const { workout } = useWorkoutState();
   const [edit, setEdit] = useState(false);
+  const navigation = useNavigation<HomeStackParamsList>();
   const hasHealthData = Boolean(workout.healthData);
 
   useEffect(() => {
@@ -25,28 +28,68 @@ const EnduranceWrapper = () => {
     });
   };
 
+  const onNavToMapView = () => {
+    if (workout.healthData) {
+      navigation.push(HomeStackScreens.Map, { data: workout.healthData });
+    }
+  };
+
   return (
     <FlexBox column flex={1} zIndex={100} margin={15} marginTop={10}>
-      {hasHealthData && (
-        <FlexBox
-          alignSelf="flex-start"
-          alignItems="center"
-          marginBottom={10}
-          padding={10}
-          borderWidth={1}
-          borderRadius={10}
-          onPress={onChangeEditHandler}
-          borderColor={Colors.white}>
-          <Icon
-            icon={edit ? 'notebook' : 'pencil'}
-            size={20}
-            color={Colors.white}
-          />
-          <PrimaryText marginLeft={5}>
-            {edit ? 'View Summary' : 'Edit Summary'}
-          </PrimaryText>
-        </FlexBox>
-      )}
+      {hasHealthData ? (
+        edit ? (
+          <FlexBox
+            borderWidth={1}
+            borderColor={Colors.white}
+            padding={6}
+            paddingRight={12}
+            paddingLeft={15}
+            borderRadius={100}
+            alignSelf="flex-end"
+            alignItems="center"
+            marginBottom={10}
+            onPress={onChangeEditHandler}>
+            <PrimaryText marginRight={5}>View Summary</PrimaryText>
+            <Icon
+              icon={'chevron'}
+              size={15}
+              color={Colors.white}
+              direction="right"
+            />
+          </FlexBox>
+        ) : (
+          <FlexBox
+            marginBottom={10}
+            justifyContent="space-between"
+            alignItems="center">
+            <FlexBox
+              borderWidth={1}
+              borderColor={Colors.white}
+              padding={6}
+              paddingRight={15}
+              paddingLeft={12}
+              borderRadius={100}
+              alignItems="center"
+              onPress={onChangeEditHandler}>
+              <Icon icon={'chevron'} size={15} color={Colors.white} />
+              <PrimaryText marginLeft={5}>Edit Summary</PrimaryText>
+            </FlexBox>
+            {hasHealthData && (
+              <FlexBox
+                onPress={onNavToMapView}
+                borderWidth={1}
+                borderColor={Colors.white}
+                padding={6}
+                paddingRight={15}
+                paddingLeft={15}
+                borderRadius={100}>
+                <PrimaryText marginRight={5}>View Map</PrimaryText>
+                <Icon icon="compass" size={20} color={Colors.white} />
+              </FlexBox>
+            )}
+          </FlexBox>
+        )
+      ) : null}
       {edit ? <EnduranceForm setEdit={setEdit} /> : <EnduranceSummary />}
     </FlexBox>
   );
