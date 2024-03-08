ec60eb7de6b057c0a2fc135f7e4ea3d876e95f95
John Nguyen <36948494+nguyening20@users.noreply.github.com>
2024-03-08 07:39:36 -0800

    default error message and duiplicates

--- app/src/services/banner/reducer.ts
+++ app/src/services/banner/reducer.ts
@@ -12,12 +12,6 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action: any) => {
   switch (action.type) {
     case SET_BANNER:
-      if (action.payload.msg.includes('There was a network error')) {
-        const exists = state.banners.find((b: any) =>
-          b.msg.includes('There was a network error'),
-        );
-        if (exists) return state;
-      }
       const newBanner = { ...action.payload, id: AutoId.newId() };
       return {
         ...state,
