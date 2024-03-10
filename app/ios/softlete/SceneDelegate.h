16749a89aeaf619b1ae6869d1379e0eddccc4d9c
John Nguyen <36948494+nguyening20@users.noreply.github.com>
2024-03-09 17:03:15 -0800

    comit softlete ios directory

---
+++ app/ios/softlete/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  softlete
+//
+//  Created by John Nguyen on 2/9/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ app/ios/softlete/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  softlete
+//
+//  Created by John Nguyen on 2/9/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
