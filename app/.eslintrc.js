ce57d02098f70c8907bcf52e9e91efeb8facec31
John Nguyen <36948494+nguyening20@users.noreply.github.com>
2024-03-21 10:12:41 -0700

    update eslint config

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,4 +1,25 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    '@react-native',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks'],
+  rules: {
+    'react/prop-types': 0,
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
 };
