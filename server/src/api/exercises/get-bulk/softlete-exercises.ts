7c5b69c14cb6b8a5c9498933340cfc5889187658
John Nguyen <36948494+nguyening20@users.noreply.github.com>
2024-04-03 07:52:09 -0700

    moving fetch-exercise-data

--- server/src/api/exercises/get-bulk/softlete-exercises.ts
+++ server/src/api/exercises/get-bulk/softlete-exercises.ts
@@ -4,54 +4,61 @@ import errorCatch from '../../../utils/error-catch';
 import mongoose from 'mongoose';
 import apicache from 'apicache';
 import cacheOnlyNonOwner from '../../../utils/cache-only-non-owner';
-import fetchExerciseData from '../helpers/fetch-exercise-data';
+import fetchExerciseData from '../../../utils/exercises/fetch-exercise-data';
 const cache = apicache.middleware;
 
-router.get('/:exUids?:userUid?', cache('10 minutes', cacheOnlyNonOwner), async (req: any, res: any, next: any) => {
+router.get(
+  '/:exUids?:userUid?',
+  cache('10 minutes', cacheOnlyNonOwner),
+  async (req: any, res: any, next: any) => {
     //user has a id token
     //verify token
     const { uid } = req.headers;
 
-    if (!uid) return res.status(401).send("cannot find user id.");
+    if (!uid) return res.status(401).send('cannot find user id.');
 
     const { exUids, userUid } = req.query;
 
-    if (userUid && typeof userUid !== 'string') return res.status(400).send('Invalid user id.');
+    if (userUid && typeof userUid !== 'string')
+      return res.status(400).send('Invalid user id.');
 
-    if (!exUids || exUids.length < 1) return res.status(400).send('Request is empty.');
+    if (!exUids || exUids.length < 1)
+      return res.status(400).send('Request is empty.');
 
     //check if a single exUid came through
     if (typeof exUids === 'string') {
-        if (!mongoose.Types.ObjectId.isValid(exUids)) return res.status(400).send('Invalid id associated with request');
+      if (!mongoose.Types.ObjectId.isValid(exUids))
+        return res.status(400).send('Invalid id associated with request');
     } else {
-        for (let i = 0; i < exUids.length; i++) {
-            if (!mongoose.Types.ObjectId.isValid(exUids[i])) return res.status(400).send('Invalid id associated with request');
-        }
+      for (let i = 0; i < exUids.length; i++) {
+        if (!mongoose.Types.ObjectId.isValid(exUids[i]))
+          return res.status(400).send('Invalid id associated with request');
+      }
     }
 
     try {
-        const softExDocs = await SoftleteExercises.find({
-            _id: { $in: exUids }
-        })
+      const softExDocs = await SoftleteExercises.find({
+        _id: { $in: exUids },
+      });
 
-        if (softExDocs.length < 1) return res.send([]);
+      if (softExDocs.length < 1) return res.send([]);
 
-        //get list of exerciseUids
-        const exUidsToFetch: mongoose.Types.ObjectId[] = [];
+      //get list of exerciseUids
+      const exUidsToFetch: mongoose.Types.ObjectId[] = [];
 
-        softExDocs.forEach(d => {
-            exUidsToFetch.push(d._id)
-        })
+      softExDocs.forEach(d => {
+        exUidsToFetch.push(d._id);
+      });
 
-        if (exUidsToFetch.length < 1) return res.send(softExDocs)
+      if (exUidsToFetch.length < 1) return res.send(softExDocs);
 
-        const exercises = await fetchExerciseData([], softExDocs, userUid)
+      const exercises = await fetchExerciseData([], softExDocs, userUid);
 
-        res.send(exercises)
-
+      res.send(exercises);
     } catch (err) {
-        return errorCatch(err, res, next)
+      return errorCatch(err, res, next);
     }
-})
+  },
+);
 
 export default router;
