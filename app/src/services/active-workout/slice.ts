27814508d62419e5b414aab14e0c6087a3047d3e
John Nguyen <36948494+nguyening20@users.noreply.github.com>
2024-04-20 15:40:21 -0700

    set up foundation of actions in workout

--- app/src/services/active-workout/slice.ts
+++ app/src/services/active-workout/slice.ts
@@ -1,8 +1,16 @@
 import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { ActiveWorkoutProps, MoveExercisePayload } from './types';
+import {
+  ActiveWorkoutProps,
+  ExerciseOrderPayload,
+  MoveExercisePayload,
+} from './types';
 import { WorkoutProps } from '@app/types';
 import axios from 'axios';
 import { PATHS, getURL } from '@app/utils';
+import { ReducerProps } from '..';
+import { setBanner } from '../banner/actions';
+import { BannerTypes } from '../banner/types';
+import { AppDispatch } from 'App';
 
 const initialState: ActiveWorkoutProps = {
   workout: undefined,
@@ -10,11 +18,62 @@ const initialState: ActiveWorkoutProps = {
 
 export const removeExerciseAsync = createAsyncThunk(
   'active-workout/remove-exercise',
-  async (exerciseUid: string) => {
-    await axios.post(getURL(PATHS.workouts.removeExercise), {
-      exerciseUid: exerciseUid,
-    });
+  async (exerciseUid: string, { dispatch, getState }) => {
+    axios
+      .post(getURL(PATHS.workouts.removeExercise), {
+        exerciseUid: exerciseUid,
+      })
+      .catch(error => {
+        console.error(error);
+        setBanner(
+          BannerTypes.error,
+          'Oops! Having trouble saving your actions.',
+        )(dispatch as AppDispatch, getState as () => ReducerProps);
+      });
     return exerciseUid;
+  },
+);
+
+export const reorderExercisesAsync = createAsyncThunk(
+  'active-workout/reorder-exercises',
+  async (payload: ExerciseOrderPayload, { dispatch, getState }) => {
+    axios
+      .put(getURL(PATHS.workouts.updateExerciseOrder), payload)
+      .catch(error => {
+        console.error(error);
+        setBanner(
+          BannerTypes.error,
+          'Oops! Having trouble saving your actions.',
+        )(dispatch as AppDispatch, getState as () => ReducerProps);
+      });
+    return payload;
+  },
+);
+
+export const moveExerciseAsync = createAsyncThunk(
+  'active-workout/move-exercise',
+  async (payload: MoveExercisePayload, { getState, dispatch }) => {
+    const rootState = getState() as ReducerProps;
+    const workoutUid = rootState.activeWorkout.workout?._id as string;
+    const requestPayload: ExerciseOrderPayload = {
+      workoutUid,
+      exercises: {
+        [payload.exerciseId]: {
+          group: payload.groupIndex,
+          order: payload.order,
+        },
+      },
+    };
+    axios
+      .put(getURL(PATHS.workouts.updateExerciseOrder), requestPayload)
+      .catch(error => {
+        console.error(error);
+        setBanner(
+          BannerTypes.error,
+          'Oops! Having trouble saving your actions.',
+        )(dispatch as AppDispatch, getState as () => ReducerProps);
+      });
+    return payload;
   },
 );
 
@@ -28,43 +87,50 @@ const activeWorkout = createSlice({
     ) => {
       state.workout = action.payload;
     },
-    moveExercise: (
-      state: ActiveWorkoutProps,
-      action: PayloadAction<MoveExercisePayload>,
-    ) => {
-      if (!state.workout) return;
-
-      const { exerciseId, groupIndex } = action.payload;
-
-      const exerciseIndex = state.workout.exercises.findIndex(
-        exercise => exercise._id === exerciseId,
-      );
-
-      if (exerciseIndex < 0) return;
-      // Directly update the group property of the exercise
-      state.workout.exercises[exerciseIndex].group = groupIndex;
-    },
     clearActiveWorkout: (state: ActiveWorkoutProps) => {
       state.workout = undefined;
     },
   },
   extraReducers: builder => {
-    builder.addCase(removeExerciseAsync.fulfilled, (state, action) => {
-      const exerciseId = action.payload;
-      if (exerciseId && state.workout) {
+    builder
+      .addCase(removeExerciseAsync.fulfilled, (state, action) => {
+        const exerciseId = action.payload;
+        if (exerciseId && state.workout) {
+          const exerciseIndex = state.workout.exercises.findIndex(
+            exercise => exercise._id === exerciseId,
+          );
+          if (exerciseIndex > -1) {
+            state.workout?.exercises.splice(exerciseIndex, 1);
+          }
+        }
+      })
+      .addCase(reorderExercisesAsync.fulfilled, (state, action) => {
+        if (!state.workout) return;
+        const updatedExercises = action.payload.exercises;
+        state.workout.exercises.forEach(exercise => {
+          const updatedProps = updatedExercises[exercise._id as string];
+          if (updatedProps) {
+            exercise.group = updatedProps.group;
+            exercise.order = updatedProps.order;
+          }
+        });
+      })
+      .addCase(moveExerciseAsync.fulfilled, (state, action) => {
+        if (!state.workout) return;
+
+        const { exerciseId, groupIndex } = action.payload;
+
         const exerciseIndex = state.workout.exercises.findIndex(
           exercise => exercise._id === exerciseId,
         );
-        if (exerciseIndex > -1) {
-          state.workout?.exercises.splice(exerciseIndex, 1);
-        }
-        state.workout = state.workout;
-      }
-    });
+
+        if (exerciseIndex < 0) return;
+        // Directly update the group property of the exercise
+        state.workout.exercises[exerciseIndex].group = groupIndex;
+      });
   },
 });
 
-export const { setActiveWorkout, clearActiveWorkout, moveExercise } =
-  activeWorkout.actions;
+export const { setActiveWorkout, clearActiveWorkout } = activeWorkout.actions;
 
 export default activeWorkout.reducer;
--- app/src/services/active-workout/types.ts
+++ app/src/services/active-workout/types.ts
@@ -7,4 +7,15 @@ export type ActiveWorkoutProps = {
 export type MoveExercisePayload = {
   exerciseId: string;
   groupIndex: number;
+  order: number;
+};
+
+export type ExerciseOrderPayload = {
+  workoutUid: string;
+  exercises: {
+    [exerciseUid: string]: {
+      group: number;
+      order: number;
+    };
+  };
 };
